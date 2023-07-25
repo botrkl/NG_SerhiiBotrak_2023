f456feba55761e1d7bdce86ee90ff21fa928a74c
botrkl <96987652+botrkl@users.noreply.github.com>
2023-07-25 23:38:48 +0300

    Implement splitter

--- Lesson1/Task4/Task4/Program.cs
+++ Lesson1/Task4/Task4/Program.cs
@@ -1,4 +1,4 @@
-﻿Console.Write("Enter first string: ");
+Console.Write("Enter first string: ");
 string str1 = Console.ReadLine();
 
 Console.Write("Enter second string: ");
@@ -10,10 +10,13 @@ Console.WriteLine("Our result: " + (resultString ?? "is null"));
 static string? sd(string str1, string str2)
 {
     if (str1.Length > str2.Length)
-        return string.Concat(str1, str2);
-    else  if (str2.Length > str1.Length)
     {
-        return null;
+        return string.Concat(str1, str2);
+    }
+    else  if (str2.Length > str1.Length && !string.IsNullOrEmpty(str1))
+    {
+        int index = str2.IndexOf(str1[0]);
+        return str2.Substring(0, index) + " " + str2.Substring(index);
     }
     return null;
 }
