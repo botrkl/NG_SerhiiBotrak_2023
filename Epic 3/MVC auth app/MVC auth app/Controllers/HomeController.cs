55109da778d9dbbf42edbacd4efddb84eefcdcae
Serhii Botrak <serhiibatrakprog@gmail.com>
2023-08-06 22:07:58 +0300

    MVC auth app

---
+++ Epic 3/MVC auth app/MVC auth app/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_auth_app.Models;
+
+namespace MVC_auth_app.Controllers
+{
+    public class HomeController : Controller
+    {
+        List<User> userList = new List<User>() 
+        { 
+            new User() { Id=1,Username="serhii", Password ="1234",IsAdmin=true},
+            new User() { Id=2,Username="volodya337", Password ="0000",IsAdmin=false }
+        };
+
+        [Route("/")]
+        [Route("/login")]
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("/login")]
+        [HttpPost]
+        public IActionResult Index(User user)
+        {
+            User? wantedUser = userList.FirstOrDefault(x => x.Username == user.Username && x.Password.Equals(user.Password));
+            if (wantedUser != null)
+            {
+                return RedirectToAction("HomePage", wantedUser);
+            }            
+            ViewBag.NonExistingAccount = "Invalid username or password!";
+            return View();
+        }
+
+        [Route("/home")]
+        public IActionResult HomePage(User user)
+        {
+            return View(user);
+        }
+    }
+}
---
+++ Epic 3/MVC auth app/MVC auth app/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MVC_auth_app.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+    }
+}
